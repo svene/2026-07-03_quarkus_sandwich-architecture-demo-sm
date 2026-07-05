806c0a9a3587abc928bed98fcbd4da4cbd3bcfbc
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-07-05 11:44:18 +0200

    cleanup

--- src/test/java/org/svenehrke/demo/GreetInAdapterIT.java
+++
@@ -1,8 +0,0 @@
-package org.svenehrke.demo;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetInAdapterIT extends GreetInAdapterTest {
-    // Execute the same tests but in packaged mode.
-}
